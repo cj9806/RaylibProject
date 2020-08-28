119bf7292bb4280b0125a56b1e3de29eff447ec3
cj9806 <69607764+cj9806@users.noreply.github.com>
2020-08-28 14:08:11 -0700

    Initialize feat-travis

---
+++ UnitTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using Raylib;
+using rl = Raylib.Raylib;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        public static int Main()
+        {
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            int screenWidth = 800;
+            int screenHeight = 450;
+
+            rl.InitWindow(screenWidth, screenHeight, "raylib [aie] - unit test");
+
+            rl.SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            rl.BeginDrawing();
+
+            rl.ClearBackground(Color.RAYWHITE);
+
+            rl.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
+
+            rl.EndDrawing();
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            rl.CloseWindow();        // Close window and OpenGL context
+                                     //--------------------------------------------------------------------------------------
+
+            return 0;
+        }
+    }
+}
